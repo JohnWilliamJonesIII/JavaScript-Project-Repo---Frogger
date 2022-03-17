54d905ab608b20c0caca1e639936cbee4cb74cdb
John William Jones III <johnwilliamjonesiii@gmail.com>
2022-03-16 23:19:05 -0500

    fixed extreme gaps between div boxes, added variables to app.js file

---
+++ app.js
@@ -0,0 +1,6 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const startPauseDisplay = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+
+
